5c1a5c18a2e50e2fe857a7996841bba9d998cbb2
Tolstykh Timur <tolstihtimur@yandex.ru>
2023-05-21 19:01:29 +0300

    fix main

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
+import alphavintage
 import coefficient
+import time
+import json
 
-tickers = []
+TIME_SLEEP = 60
 
-file_path = 'top50.txt'
+def update_dataset():
+    tickers = []
+    file_path = 'top3.txt'
 
-with open(file_path, 'r') as file:
-    for line in file:
-        ticker = line.strip() 
-        tickers.append(ticker)
+    with open(file_path, 'r') as file:
+        for line in file:
+            ticker = line.strip() 
+            tickers.append(ticker)
+    
+    tickers_dict = []
+    for ticker in tickers:
+        info = alphavintage.get_info_by_ticker(ticker=ticker)
+        tickers_dict.append(info)
+        time.sleep(TIME_SLEEP)
+    
+    with open('dataset.json', 'w') as file:
+        json.dump(tickers_dict, file)
 
-for ticker in tickers:
-    # pe_ratio = coefficient.pe_ratio(ticker=ticker)
-    # pb_ratio = coefficient.pb_ratio(ticker=ticker)
-    # ps_ratio = coefficient.ps_ratio(ticker=ticker)
-    dataset = coefficient.alphavintage.get_info_by_ticker(ticker=ticker) #Delete
-    graham_number = coefficient.get_graham_number(dataset)
-    graham_percent = coefficient.get_graham_percent(dataset)
-    print(f"{ticker}, GP: {graham_percent}, GN: {graham_number}") 
+update_required = input("Нужно ли обновить датасет? (да/нет): ").lower() == 'да'
+
+if update_required:
+    update_dataset()
+    print("Датасет успешно обновлен.")
+
+with open('dataset.json', 'r') as file:
+    stocks = json.load(file)
+
+
+for stock in stocks:
+    graham_number = coefficient.get_graham_number(stock)
+    graham_percent = coefficient.get_graham_percent(stock)
+    print(f"{stock['ticker']}: {graham_percent}, {graham_number}, {stock['assets']}, {stock['debt']}, {stock['shares_outstanding']}, {stock['current_price']}")
+    
