a81c14044a79688ff6963c19db8e3d5f814bd1ad
almostinf <almostinf@mail.ru>
2023-05-21 17:57:37 +0300

    added dataset and updated requirements.txt

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
+import alphavintage
 import coefficient
+import time
+import json
 
-tickers = []
+TIME_SLEEP = 60
 
-file_path = 'top50.txt'
+def update_dataset():
+    tickers = []
+    file_path = 'top3.txt'
 
-with open(file_path, 'r') as file:
-    for line in file:
-        ticker = line.strip() 
-        tickers.append(ticker)
+    with open(file_path, 'r') as file:
+        for line in file:
+            ticker = line.strip() 
+            tickers.append(ticker)
+    
+    tickers_dict = []
+    for ticker in tickers:
+        info = alphavintage.get_info_by_ticker(ticker=ticker)
+        tickers_dict.append(info)
+        time.sleep(TIME_SLEEP)
+    
+    with open('dataset.json', 'w') as file:
+        json.dump(tickers_dict, file)
 
-for ticker in tickers:
-    pe_ratio = coefficient.pe_ratio(ticker=ticker)
-    pb_ratio = coefficient.pb_ratio(ticker=ticker)
-    ps_ratio = coefficient.ps_ratio(ticker=ticker)
+update_required = input("Нужно ли обновить датасет? (да/нет): ").lower() == 'да'
+
+if update_required:
+    update_dataset()
+    print("Датасет успешно обновлен.")
+
+with open('dataset.json', 'r') as file:
+    stocks = json.load(file)
+    
+for stock in stocks:
+    pe_ratio = stock['pe_ratio']
+    ps_ratio = stock['ps_ratio']
+    pb_ratio = stock['pb_ratio']
+    
