a92292a2153faa6d927b640edd8ff93b3695b52b
almostinf <almostinf@mail.ru>
2023-05-25 18:54:19 +0300

    fixed all graphics

--- graphic.py
+++ graphic.py
@@ -11,8 +11,8 @@ def set_data(stocks):
     for stock in stocks:
         tickers.append(stock['ticker'])
         pe_ratios.append(stock['pe_ratio'])
-        pb_ratios.append(stock['pe_ratio'])
-        ps_ratios.append(stock['pe_ratio'])
+        pb_ratios.append(stock['pb_ratio'])
+        ps_ratios.append(stock['ps_ratio'])
         market_caps.append(stock['market_cap'])
         volumes.append(stock['volume'])
 
@@ -33,21 +33,27 @@ def print_charts(stocks):
     plt.figure(2)
     plt.subplot(1, 3, 1)
     plt.scatter(market_caps, pe_ratios, s=100, alpha=0.5)
+    for i in range(len(tickers)):
+        plt.text(market_caps[i], pe_ratios[i], tickers[i])
     plt.title('Market Cap vs. P/E Ratio')
     plt.xlabel('Market Cap (in billions)')
     plt.ylabel('P/E Ratio')
     
     plt.subplot(1, 3, 2)
-    plt.scatter(market_caps, pe_ratios, s=100, alpha=0.5)
-    plt.title('Market Cap vs. P/E Ratio')
+    plt.scatter(market_caps, ps_ratios, s=100, alpha=0.5)
+    for i in range(len(tickers)):
+        plt.text(market_caps[i], ps_ratios[i], tickers[i])
+    plt.title('Market Cap vs. P/S Ratio')
     plt.xlabel('Market Cap (in billions)')
-    plt.ylabel('P/E Ratio')
+    plt.ylabel('P/S Ratio')
     
     plt.subplot(1, 3, 3)
-    plt.scatter(market_caps, pe_ratios, s=100, alpha=0.5)
-    plt.title('Market Cap vs. P/E Ratio')
+    plt.scatter(market_caps, pb_ratios, s=100, alpha=0.5)
+    for i in range(len(tickers)):
+        plt.text(market_caps[i], pb_ratios[i], tickers[i])
+    plt.title('Market Cap vs. P/B Ratio')
     plt.xlabel('Market Cap (in billions)')
-    plt.ylabel('P/E Ratio')
+    plt.ylabel('P/B Ratio')
     
     plt.tight_layout()
     plt.show()
